754cb5c313966741985ea64b62f54327a66c616a
Sebastian Krajewski <mail@wieku.me>
2017-12-05 21:14:07 +0100

    Removed unecessary data

--- core/src/me/wieku/circuits/api/element/BasicGate.kt
+++ core/src/me/wieku/circuits/api/element/BasicGate.kt
@@ -2,16 +2,14 @@ package me.wieku.circuits.api.element
 
 import me.wieku.circuits.api.math.Axis
 import me.wieku.circuits.api.math.Vector2i
+import me.wieku.circuits.api.state.State
 import me.wieku.circuits.api.world.IWorld
 import java.util.*
 
 abstract class BasicGate(pos: Vector2i): BasicElement(pos),ITickable {
 
 	protected val inputs = ArrayList<BasicInput>()
-
-	private val outputs = ArrayList<BasicWire>()
-	private val axes = ArrayList<Axis>()
-	protected var dirty = true
+	private val outputs = ArrayList<State>()
 
 	override fun onPlace(world: IWorld) {
 		updateIO(world)
@@ -19,24 +17,21 @@ abstract class BasicGate(pos: Vector2i): BasicElement(pos),ITickable {
 
 	override fun onNeighbourChange(position: Vector2i, world: IWorld) {
 		updateIO(world)
-		dirty = true
 	}
 
 	protected fun setOut(value: Boolean) {
-		for(i in 0 until outputs.size) outputs[i].getState(axes[i]).setActive(value)
+		for(i in 0 until outputs.size) outputs[i].setActive(value)
 	}
 
 	private fun updateIO(world: IWorld) {
 		inputs.clear()
 		outputs.clear()
-		axes.clear()
 
 		world.getNeighboursOf(this) {
 			when(it) {
 				is BasicInput -> inputs += it
 				is BasicWire -> {
-					outputs += it
-					axes += Axis.getAxis(getPosition(), it.getPosition())
+					outputs += it.getState(Axis.getAxis(getPosition(), it.getPosition()))
 				}
 			}
 		}
