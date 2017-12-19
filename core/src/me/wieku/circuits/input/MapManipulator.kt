f42873e3c08de5f490bb6d7ff6f18e4e68cdcaeb
Sebastian Krajewski <mail@wieku.me>
2017-12-19 02:44:35 +0100

    Input handler "rewrite"

--- core/src/me/wieku/circuits/input/MapManipulator.kt
+++ core/src/me/wieku/circuits/input/MapManipulator.kt
@@ -4,19 +4,28 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.math.Bresenham2
 import com.badlogic.gdx.math.Vector3
-import com.badlogic.gdx.scenes.scene2d.Stage
 import me.wieku.circuits.api.math.Rectangle
 import me.wieku.circuits.api.math.Vector2i
 import me.wieku.circuits.render.scene.fit
 import me.wieku.circuits.render.screens.Editor
+import me.wieku.circuits.utils.Bresenham
 import me.wieku.circuits.world.ClassicWorld
 import me.wieku.circuits.world.ElementRegistry
 import me.wieku.circuits.world.WorldClipboard
 
-//TODO: Clean this mess
 class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val editor: Editor):InputProcessor {
+
+	private enum class STATE {
+		NONE,
+		DRAWING,
+		LINE,
+		AXIS_LINE,
+		SELECTION,
+		PASTE,
+		MOVE,
+		MOVE_PASTE;
+	}
 
 	var toPlace = "wire"
 	private var last = Vector2i(-1, -1)
@@ -27,19 +36,95 @@ class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val
 	private var beginTMP = Vector2i()
 	private var endTMP = Vector2i()
 
-
 	var rectangle:Rectangle? = null
 	var clipboard:WorldClipboard? = null
 
 	var pasteMode = false
 	var lineMode = false
-	private var afterOperation = false
 
 	private var lastTooltip = ""
 
+	private var brushState = STATE.NONE
+
+	override fun keyDown(keycode: Int): Boolean {
+		if(editor.stage.keyDown(keycode)) return true
+
+		if(brushState == STATE.SELECTION){
+			when (keycode) {
+				Input.Keys.C -> makeCopy()
+				Input.Keys.X -> makeCut()
+				Input.Keys.V -> makePaste()
+				Input.Keys.S -> editor.saveFile()
+			}
+		}
+
+		if((brushState == STATE.PASTE || brushState == STATE.MOVE_PASTE) && (keycode == Input.Keys.SHIFT_LEFT || keycode == Input.Keys.SHIFT_RIGHT)) {
+			brushState = STATE.MOVE_PASTE
+			lastMX = Gdx.input.x
+			lastMY = Gdx.input.y
+			return false
+		}
+
+		if(brushState != STATE.NONE) return false
+
+		if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
+			brushState = STATE.SELECTION
+		} else if(keycode == Input.Keys.F) {
+			if(rectangle != null) {
+				world.fill(rectangle!!, toPlace)
+			}
+		} else if(rectangle != null && keycode == Input.Keys.FORWARD_DEL) {
+			world.clear(rectangle!!)
+		} else if(keycode == Input.Keys.A) {
+			brushState = STATE.LINE
+		} else if(keycode == Input.Keys.D) {
+			brushState = STATE.AXIS_LINE
+		} else if (keycode == Input.Keys.SHIFT_LEFT || keycode == Input.Keys.SHIFT_RIGHT) {
+			lastMX = Gdx.input.x
+			lastMY = Gdx.input.y
+			brushState = STATE.MOVE
+		}
+
+		return false
+	}
+
+	override fun keyUp(keycode: Int): Boolean {
+		if(editor.stage.keyUp(keycode)) return true
+		if(keycode == Input.Keys.A || keycode == Input.Keys.D) {
+			if(!Gdx.input.isButtonPressed(Input.Buttons.LEFT) && !Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+				if(keycode == Input.Keys.A && Gdx.input.isKeyPressed(Input.Keys.D)) {
+					brushState = STATE.AXIS_LINE
+				} else if(Gdx.input.isKeyPressed(Input.Keys.A) && keycode == Input.Keys.D) {
+					brushState = STATE.LINE
+				} else {
+					lineMode = false
+					brushState = STATE.NONE
+				}
+			}
+		}
+
+		if(keycode == Input.Keys.CONTROL_LEFT) {
+			if(lastTooltip != "") {
+				editor.tooltip.hide()
+				lastTooltip = ""
+			}
+			if(brushState == STATE.SELECTION && !Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+				brushState = STATE.NONE
+			}
+		}
+
+		if((brushState == STATE.MOVE_PASTE || brushState == STATE.MOVE) && (keycode == Input.Keys.SHIFT_LEFT && !Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT)) || (!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) && keycode == Input.Keys.SHIFT_RIGHT)) {
+			brushState = if(brushState == STATE.MOVE_PASTE) {
+				STATE.PASTE
+			} else STATE.NONE
+		}
+
+		return false
+	}
+
 	override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
 		if(editor.stage.mouseMoved(screenX, screenY)) return true
-		var vec = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+		val vec = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
 		position.set(vec.x.toInt(), vec.y.toInt())
 
 		if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
@@ -66,125 +151,19 @@ class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val
 		}
 	}
 
-	override fun keyTyped(character: Char): Boolean {
-		if(editor.stage.keyTyped(character)) return true
-		return false
-	}
-
-	override fun scrolled(amount: Int): Boolean {
-		if(editor.stage.scrolled(amount)) return true
-		var before = camera.unproject(Vector3(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f))
-		camera.zoom = Math.max(0.01f, Math.min(4f, camera.zoom+amount*camera.zoom*0.1f))
-		camera.update()
-		var after = camera.unproject(Vector3(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f))
-		camera.position.sub(after.sub(before))
-		camera.fit(world, editor.stage)
-		return false
-	}
-
-	override fun keyUp(keycode: Int): Boolean {
-		if(editor.stage.keyUp(keycode)) return true
-		if(keycode == Input.Keys.A || keycode == Input.Keys.D) {
-			lineMode = false
-		}
-
-		if(keycode == Input.Keys.CONTROL_LEFT) {
-			if(lastTooltip != "") {
-				editor.tooltip.hide()
-				lastTooltip = ""
-			}
-		}
-
-		return false
-	}
-
-	override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
-		if(editor.stage.touchDragged(screenX, screenY, pointer)) return true
-
-		if(Gdx.input.isKeyPressed(Input.Keys.A) && afterOperation) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
-			return true
-		} else if(Gdx.input.isKeyPressed(Input.Keys.D) && afterOperation) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
-			var angle = endPos.copy().sub(beginPos).angle()
-
-			if(angle >= 315 || angle <=45 || (angle in 135.0..225.0)) {
-				endPos.set(endPos.x, beginPos.y)
-			} else {
-				endPos.set(beginPos.x, endPos.y)
-			}
-			return true
-		}
-
-		if(!pasteMode && Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			if(Gdx.input.isButtonPressed(Input.Buttons.LEFT) && rectangle != null) {
-
-				endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
-
-				beginTMP.set(beginPos)
-				endTMP.set(endPos)
-
-				calculate(beginTMP, endTMP)
-
-				rectangle!!.reshape(beginTMP, endTMP)
-			}
-
-			tooltip(Vector2i(upr.x.toInt(), upr.y.toInt()))
-		} else {
-			processTouch(screenX, screenY, pointer, true)
-		}
-		return false
-	}
-
-	private fun calculate(beginPosition: Vector2i, endPosition: Vector2i) {
-		if(endPosition.x < beginPosition.x) {
-			beginPosition.add(1, 0)
-		} else {
-			endPosition.add(1, 0)
-		}
-
-		if(endPosition.y < beginPosition.y) {
-			beginPosition.add(0, 1)
-		} else {
-			endPosition.add(0, 1)
-		}
-	}
-
-	override fun keyDown(keycode: Int): Boolean {
-		if(editor.stage.keyDown(keycode)) return true
-		if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
-			if(Gdx.input.isKeyPressed(Input.Keys.C)) {
-				makeCopy()
-			} else if(Gdx.input.isKeyPressed(Input.Keys.X)){
-				makeCut()
-			} else if(Gdx.input.isKeyPressed(Input.Keys.V)){
-				makePaste()
-			} else if(Gdx.input.isKeyPressed(Input.Keys.S)){
-				editor.saveFile()
-			}
-		} else if(Gdx.input.isKeyPressed(Input.Keys.F)) {
-			if(rectangle != null){
-				world.fill(rectangle!!, toPlace)
-			}
-		}	else if(rectangle != null && Gdx.input.isKeyPressed(Input.Keys.FORWARD_DEL)) {
-			world.clear(rectangle!!)
-		}
-		return false
-	}
-
 	override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
 		if(editor.stage.touchUp(screenX, screenY, pointer, button)) return true
-		if(Gdx.input.isKeyPressed(Input.Keys.A) && afterOperation) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
+
+		var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+		endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
+
+		if(brushState == STATE.LINE) {
 			lineMode = false
 			drawLine(beginPos, endPos, true)
-		} else if(Gdx.input.isKeyPressed(Input.Keys.D) && afterOperation) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
+			if(!Gdx.input.isKeyPressed(Input.Keys.A)) {
+				brushState = if(Gdx.input.isKeyPressed(Input.Keys.D)) STATE.AXIS_LINE else STATE.NONE
+			}
+		} else if(brushState == STATE.AXIS_LINE) {
 			var angle = endPos.copy().sub(beginPos).angle()
 
 			if(angle >= 315 || angle <=45 || (angle in 135.0..225.0)) {
@@ -194,33 +173,36 @@ class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val
 			}
 			lineMode = false
 			drawLine(beginPos, endPos, true)
-		} else if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) && button == Input.Buttons.LEFT && rectangle != null) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
-
-			beginTMP.set(beginPos)
-			endTMP.set(endPos)
-
-			calculate(beginTMP, endTMP)
-
+			if(!Gdx.input.isKeyPressed(Input.Keys.D)) {
+				brushState = if(Gdx.input.isKeyPressed(Input.Keys.A)) STATE.LINE else STATE.NONE
+			}
+		} else if(brushState == STATE.SELECTION && button == Input.Buttons.LEFT && rectangle != null) {
+			calculateSelection(beginTMP.set(beginPos), endTMP.set(endPos))
 			rectangle!!.reshape(beginTMP, endTMP)
+
+			if(!Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) && !Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) {
+				brushState = STATE.NONE
+			}
+		} else if(brushState == STATE.DRAWING) {
+			brushState = STATE.NONE
 		}
-		afterOperation = false
 		return false
 	}
 
+
+	private var lastMX = -1
+	private var lastMY = -1
 	override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
 		if(editor.stage.touchDown(screenX, screenY, pointer, button)) return true
-
-		if(Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.D)) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+		lastMX = screenX
+		lastMY = screenY
+		println(brushState)
+		val upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+		if(brushState == STATE.LINE || brushState == STATE.AXIS_LINE) {
 			beginPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
-			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
-			afterOperation = true
+			endPos.set(beginPos)
 			lineMode = true
-			return false
-		} else if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
-			val upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+		} else if(brushState == STATE.SELECTION) {
 			if(button == Input.Buttons.LEFT) {
 				beginPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
 				endPos.set(beginPos)
@@ -229,65 +211,92 @@ class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val
 				val element = world.getElement(Vector2i(upr.x.toInt(), upr.y.toInt()))
 				if(element != null)
 					editor.editElement(element)
-				return false
 			}
-		} else if(pasteMode) {
-			if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-				if(button == Input.Buttons.LEFT) {
-					world.paste(position.copy().sub(clipboard!!.width/2, clipboard!!.height/2), clipboard!!)
-					pasteMode = false
-					afterOperation = true
-				} else {
-					pasteMode = false
-					afterOperation = true
+		} else if(brushState == STATE.PASTE) {
+			if(button == Input.Buttons.LEFT) {
+				world.paste(position.copy().sub(clipboard!!.width/2, clipboard!!.height/2), clipboard!!)
+			}
+			pasteMode = false
+			brushState = STATE.NONE
+		} else if(brushState == STATE.NONE) {
+			if(rectangle != null) {
+				rectangle = null
+			} else if(button == Input.Buttons.MIDDLE) {
+				val element = world.getElement(Vector2i(upr.x.toInt(), upr.y.toInt()))
+				if(element != null) {
+					toPlace = ElementRegistry.names[element.javaClass]!!
 				}
-				return false
+			} else {
+				brushState = STATE.DRAWING
+
+				last.set(position.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1))
+
+				if(button == Input.Buttons.LEFT) {
+					drawLine(last, position, true)
+				} else if(button == Input.Buttons.RIGHT) {
+					drawLine(last, position, false)
+				}
 			}
-		} else if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) && rectangle != null) {
-			rectangle = null
-			afterOperation = true
-			return false
+		} else if(brushState == STATE.MOVE || brushState == STATE.MOVE_PASTE) {
+			lastMX = screenX
+			lastMY = screenY
 		}
-		if(button == Input.Buttons.MIDDLE) {
-			var upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			var element = world.getElement(Vector2i(upr.x.toInt(), upr.y.toInt()))
-			if(element != null) {
-				toPlace = ElementRegistry.names[element.javaClass]!!
-			}
-		}
-		processTouch(screenX, screenY, pointer, false)
+
 		return false
 	}
 
-	private fun processTouch(screenX: Int, screenY: Int, pointer: Int, dragging: Boolean) {
-		if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-			camera.position.sub(Gdx.input.getDeltaX(pointer)*camera.zoom, Gdx.input.getDeltaY(pointer)*camera.zoom, 0f)
-			camera.fit(world, editor.stage)
-			afterOperation = true
-		} else {
-			val upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
-			val res = Vector2i(upr.x.toInt(), upr.y.toInt())
-			position.set(res)
-			if(afterOperation || pasteMode || (rectangle != null)) return
-			if(dragging) {
-				if(res == last)
-					return
-			} else last.set(res)
-			if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-				drawLine(last, res, true)
-			} else if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
-				drawLine(last, res, false)
+	override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+		if(editor.stage.touchDragged(screenX, screenY, pointer)) return true
+		val upr = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+
+		if(brushState == STATE.LINE || brushState == STATE.AXIS_LINE) {
+			endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
+			if(brushState == STATE.AXIS_LINE) {
+				val angle = endPos.copy().sub(beginPos).angle()
+
+				if(angle >= 315 || angle <=45 || (angle in 135.0..225.0)) {
+					endPos.set(endPos.x, beginPos.y)
+				} else {
+					endPos.set(beginPos.x, endPos.y)
+				}
 			}
-			last.set(res)
+		} else if(brushState == STATE.SELECTION && Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) {
+			if(Gdx.input.isButtonPressed(Input.Buttons.LEFT) && rectangle != null) {
+				endPos.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
+
+				beginTMP.set(beginPos)
+				endTMP.set(endPos)
+
+				calculateSelection(beginTMP, endTMP)
+
+				rectangle!!.reshape(beginTMP, endTMP)
+			}
+
+			tooltip(Vector2i(upr.x.toInt(), upr.y.toInt()))
+		} else if(brushState == STATE.MOVE || brushState == STATE.MOVE_PASTE) {
+			camera.position.sub((screenX-lastMX)*camera.zoom, (screenY-lastMY)*camera.zoom, 0f)
+			camera.fit(world, editor.stage)
+			lastMX = screenX
+			lastMY = screenY
+		} else if(brushState == STATE.DRAWING) {
+			position.set(upr.x.toInt(), upr.y.toInt()).clamp(0, 0, world.width-1, world.height-1)
+			if(position != last) {
+				if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+					drawLine(last, position, true)
+				} else if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
+					drawLine(last, position, false)
+				}
+				last.set(position)
+			}
 		}
+		return false
 	}
 
-	private val bresenham = Bresenham2()
 	private fun drawLine(from: Vector2i, to: Vector2i, place: Boolean) {
 		if(from == to) {
 			makeAction(from.x, from.y, place)
 		} else {
-			bresenham.line(from.x, from.y, to.x, to.y).forEach {
+			Bresenham.iterateFast(from, to) {
 				makeAction(it.x, it.y, place)
 			}
 		}
@@ -299,6 +308,11 @@ class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val
 		} else {
 			world.removeElement(Vector2i(posx, posy))
 		}
+	}
+
+	private fun calculateSelection(beginPosition: Vector2i, endPosition: Vector2i) {
+		beginPosition.add(if(endPosition.x < beginPosition.x) 1 else 0, if(endPosition.y < beginPosition.y) 1 else 0)
+		endPosition.add(if(endPosition.x < beginPosition.x) 0 else 1, if(endPosition.y < beginPosition.y) 0 else 1)
 	}
 
 	fun makeCut() {
@@ -317,7 +331,24 @@ class MapManipulator(val world:ClassicWorld, val camera: OrthographicCamera, val
 	fun makePaste() {
 		if(clipboard != null) {
 			pasteMode = true
+			brushState = STATE.PASTE
 		}
+	}
+
+	override fun keyTyped(character: Char): Boolean {
+		if(editor.stage.keyTyped(character)) return true
+		return false
+	}
+
+	override fun scrolled(amount: Int): Boolean {
+		if(editor.stage.scrolled(amount)) return true
+		val before = camera.unproject(Vector3(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f))
+		camera.zoom = Math.max(0.01f, Math.min(4f, camera.zoom+amount*camera.zoom*0.1f))
+		camera.update()
+		val after = camera.unproject(Vector3(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f))
+		camera.position.sub(after.sub(before))
+		camera.fit(world, editor.stage)
+		return false
 	}
 
 }
